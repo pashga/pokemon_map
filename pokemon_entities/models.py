afa69e42aec18785d138f4209adfdde15a1efce1
pashga <pashga112@gmail.com>
2025-07-30 21:08:55 +0300

    show pokemons on mainpage.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,11 +1,11 @@
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
+    title_ru = models.CharField(max_length=200)
     image = models.ImageField(null=True)
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.title_ru}"
 
 
 class PokemonEntity(models.Model):
