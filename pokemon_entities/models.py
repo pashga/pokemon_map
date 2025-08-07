80a627336b099c5a785758a69d195b6c0e63f9ac
pashga <pashga112@gmail.com>
2025-08-07 19:31:21 +0300

    Added blank and null fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,15 +2,21 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200,
-                                verbose_name="Название на русском"
+                                verbose_name="Название на русском",
+                                blank=True
                                 )
-    image = models.ImageField(null=True, verbose_name="Изображение")
-    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(blank=True,
+                              null=True,
+                              verbose_name="Изображение",
+                              )
+    description = models.TextField(blank=True, verbose_name="Описание")
     title_en = models.CharField(max_length=200,
-                                verbose_name="Название на английском"
+                                verbose_name="Название на английском",
+                                blank=True,
                                 )
     title_jp = models.CharField(max_length=200,
-                                verbose_name="Название на японском"
+                                verbose_name="Название на японском",
+                                blank=True,
                                 )
     previous_evolution = models.ForeignKey(
         "self",
@@ -26,19 +32,40 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    lat = models.FloatField(verbose_name="Широта")
+    lat = models.FloatField(verbose_name="Широта",)
     lon = models.FloatField(verbose_name="Долгота")
-    appeared_at = models.DateTimeField(verbose_name="Появляется")
-    disappeared_at = models.DateTimeField(verbose_name="Исчезает")
+    appeared_at = models.DateTimeField(verbose_name="Появляется",
+                                       blank=True,
+                                       null=True,
+                                       )
+    disappeared_at = models.DateTimeField(verbose_name="Исчезает",
+                                          blank=True,
+                                          null=True,
+                                          )
     pokemon = models.ForeignKey(Pokemon,
                                 on_delete=models.CASCADE,
                                 verbose_name="Название",
                                 )
-    level = models.IntegerField(verbose_name="Уровень")
-    health = models.IntegerField(verbose_name="Здоровье")
-    strength = models.IntegerField(verbose_name="Сила")
-    defence = models.IntegerField(verbose_name="Защита")
-    stamina = models.IntegerField(verbose_name="Выносливость")
+    level = models.IntegerField(verbose_name="Уровень",
+                                blank=True,
+                                null=True,
+                                )
+    health = models.IntegerField(verbose_name="Здоровье",
+                                 blank=True,
+                                 null=True,
+                                 )
+    strength = models.IntegerField(verbose_name="Сила",
+                                   blank=True,
+                                   null=True,
+                                   )
+    defence = models.IntegerField(verbose_name="Защита",
+                                  blank=True,
+                                  null=True,
+                                  )
+    stamina = models.IntegerField(verbose_name="Выносливость",
+                                  blank=True,
+                                  null=True,
+                                  )
 
     def __str__(self):
         return f"{self.pokemon}"
